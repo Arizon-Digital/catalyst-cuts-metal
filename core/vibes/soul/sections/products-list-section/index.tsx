152ee5208dab93b58e8a0f2b9cb6734fdba33f78
priyanka-patra98 <priyanka@arizon.digital>
2025-02-28 15:26:17 +0530

    overlapping in plp

--- core/vibes/soul/sections/products-list-section/index.tsx
+++ core/vibes/soul/sections/products-list-section/index.tsx
@@ -70,7 +70,7 @@ export function ProductsListSection({
 }: Props) {
   return (
     <div className="group/products-list-section @container">
-      <div className="mx-auto max-w-screen-2xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-12">
+      <div className="mx-auto max-w-screen-2xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:pt-12 pb-[6rem]">
         <div>
           <Stream fallback={<BreadcrumbsSkeleton />} value={streamableBreadcrumbs}>
             {(breadcrumbs) =>
